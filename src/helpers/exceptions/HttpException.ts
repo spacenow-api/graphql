db581b4f6833f94eabe2db993c4ec282d1ed1fc3
Arthemus <arthemus.moreira@gmail.com>
2019-07-19 15:10:23 +1000

    Fixing exception return.

--- src/helpers/exceptions/HttpException.ts
+++ src/helpers/exceptions/HttpException.ts
@@ -1,16 +1,14 @@
 import { IDataSourceErr } from './exception.interface'
 
-export const toError = (err: IDataSourceErr) => {
+export const toError = (err: IDataSourceErr): string => {
   console.debug('New exception unmarshal: ', err);
-  return err.extensions.response.body;
+  const errBody = err.extensions.response.body;
+  return errBody.message || 'API exception not mapped.';
 }
 
 export default class HttpException extends Error {
-
   status: number;
-
   message: string;
-
   constructor(status: number, message: string) {
     super(message);
     this.status = status;
