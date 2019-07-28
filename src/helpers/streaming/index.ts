4b234404f210cc02711f9a53e52c8d7129c5af0d
Lucas <csalucasnascimento@gmail.com>
2019-07-29 09:02:11 +1000

    folder creation through API

--- src/helpers/streaming/index.ts
+++ src/helpers/streaming/index.ts
@@ -1,22 +1,24 @@
-import fs from 'fs';
-import * as config from '../../config';
+import fs from "fs";
+import * as config from "../../config";
 
-const UPLOAD_DIR = './temp';
+if (!fs.existsSync(`${config.TEMP_FILE_UPLOAD}`)) {
+  fs.mkdirSync(`${config.TEMP_FILE_UPLOAD}`);
+}
 
 const streaming = ({ stream, filename }: any) => {
-	const path = `${config.TEMP_FILE_UPLOAD}/${filename}`;
-	return new Promise((resolve, reject) =>
-		stream
-			.on('error', (error: any) => {
-				if (stream.truncated)
-					// Delete the truncated file.
-					fs.unlinkSync(path);
-				reject(error);
-			})
-			.pipe(fs.createWriteStream(path))
-			.on('error', (error: any) => reject(error))
-			.on('finish', () => resolve({ path })),
-	);
+  const path = `${config.TEMP_FILE_UPLOAD}/${filename}`;
+  return new Promise((resolve, reject) =>
+    stream
+      .on("error", (error: any) => {
+        if (stream.truncated)
+          // Delete the truncated file.
+          fs.unlinkSync(path);
+        reject(error);
+      })
+      .pipe(fs.createWriteStream(path))
+      .on("error", (error: any) => reject(error))
+      .on("finish", () => resolve({ path }))
+  );
 };
 
 export default streaming;
