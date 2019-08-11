0f7c9ddd6a22c6557bca0225ef75a035ceda48fc
Lucas <csalucasnascimento@gmail.com>
2019-08-12 09:52:18 +1000

    delete user by email

--- src/services/api-users.ts
+++ src/services/api-users.ts
@@ -1,33 +1,37 @@
-import { IUser } from '../interfaces';
-import PersonalizationAPI from '../interfaces/personalization.inteface';
+import { IUser } from "../interfaces";
+import PersonalizationAPI from "../interfaces/personalization.inteface";
 
 class UsersAPI extends PersonalizationAPI {
-	private path = '/users';
+  private path = "/users";
 
-	constructor(apiAddress: string) {
-		super();
-		this.baseURL = apiAddress;
-	}
+  constructor(apiAddress: string) {
+    super();
+    this.baseURL = apiAddress;
+  }
 
-	getAllUsers = async (): Promise<[IUser]> => {
-		return this.get(`${this.path}`);
-	};
+  getAllUsers = async (): Promise<[IUser]> => {
+    return this.get(`${this.path}`);
+  };
 
-	getAllUsersLegacy = async (): Promise<[IUser]> => {
-		return this.get(`${this.path}/legacy`);
-	};
+  getAllUsersLegacy = async (): Promise<[IUser]> => {
+    return this.get(`${this.path}/legacy`);
+  };
 
-	getUser = async (id: string): Promise<IUser> => {
-		return this.get(`${this.path}/${id}`);
-	};
+  getUser = async (id: string): Promise<IUser> => {
+    return this.get(`${this.path}/${id}`);
+  };
 
-	createUser = async (user: IUser): Promise<IUser> => {
-		return this.post(`${this.path}`, user);
-	};
+  createUser = async (user: IUser): Promise<IUser> => {
+    return this.post(`${this.path}`, user);
+  };
 
-	updateUserLegacy = async (user: IUser): Promise<IUser> => {
-		return this.patch(`${this.path}/legacy?id=${user.id}`, user);
-	};
+  updateUserLegacy = async (user: IUser): Promise<IUser> => {
+    return this.patch(`${this.path}/legacy?id=${user.id}`, user);
+  };
+
+  deleteUserByEmail = async (email: string): Promise<IUser> => {
+    return this.delete(`${this.path}/legacy?email=${email}`);
+  };
 }
 
 export default UsersAPI;
