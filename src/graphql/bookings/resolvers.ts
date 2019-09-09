fe522219b4cc64adff59316924c10133ebac02dd
Arthemus <arthemus.moreira@gmail.com>
2019-09-09 15:29:34 +1000

    Export get booking by id function.

--- src/graphql/bookings/resolvers.ts
+++ src/graphql/bookings/resolvers.ts
@@ -1,43 +1,39 @@
 import GraphQLUpload from "graphql-upload";
 
 const resolvers = {
+
   Upload: GraphQLUpload,
 
   BookingType: {
+
     listing(parent: any, args: any, context: any, info: any) {
       return context.dataSources.listingsAPI.getListingById(parent.listingId);
     }
   },
 
   Query: {
-    getPendingBookingsByUser: async (
-      _: any,
-      args: any,
-      { dataSources }: any
-    ) => {
-      const { bookingsAPI } = dataSources;
-      console.log(args);
-      return await bookingsAPI.getPendingBookingsByUser(args);
+
+    getBookingById: async (_: any, args: any, { dataSources }: any) => {
+      return dataSources.bookingsAPI.getBookingById(args.id);
+    },
+
+    getPendingBookingsByUser: async (_: any, args: any, { dataSources }: any) => {
+      return dataSources.bookingsAPI.getPendingBookingsByUser(args);
     },
 
     getAllBookingsByUser: async (_: any, args: any, { dataSources }: any) => {
-      const { bookingsAPI } = dataSources;
-      console.log(args);
-      return await bookingsAPI.getAllBookingsByUser(args);
+      return dataSources.bookingsAPI.getAllBookingsByUser(args);
     }
   },
 
   Mutation: {
-    createBooking: async (_: any, args: any, { dataSources }: any) => {
-      const { bookingsAPI } = dataSources;
 
-      return bookingsAPI.createBooking(args);
+    createBooking: async (_: any, args: any, { dataSources }: any) => {
+      return dataSources.bookingsAPI.createBooking(args);
     },
 
     timeoutBooking: async (_: any, args: any, { dataSources }: any) => {
-      const { bookingsAPI } = dataSources;
-
-      return bookingsAPI.timeoutBooking(args.bookingId);
+      return dataSources.bookingsAPI.timeoutBooking(args.bookingId);
     }
   }
 };
