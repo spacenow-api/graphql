d5f3dc18253909e57970a0db7ebeccd9229f8187
Arthemus <arthemus.moreira@gmail.com>
2019-09-10 16:08:47 +1000

    Fixing variable.

--- src/services/api-bookings.ts
+++ src/services/api-bookings.ts
@@ -13,33 +13,33 @@ class BookingsAPI extends PersonalizationAPI {
   }
 
   getAllBookings = async (): Promise<[IBooking]> => {
-    return this.get(`/bookings`).catch(err => new ApolloError(toError(err)));
+    return this.get(`/`).catch(err => new ApolloError(toError(err)));
   };
 
   getBookingById = async (id: string): Promise<IBooking> => {
-    return this.get(`/bookings/${id}`).catch(err => new ApolloError(toError(err)));
+    return this.get(`/${id}`).catch(err => new ApolloError(toError(err)));
   };
 
   cleanListingAvailabilities = async (id: string): Promise<IBooking> => {
-    return await this.put(`/bookings/cleanListingAvailabilities/${id}`).catch(err => new ApolloError(toError(err)));
+    return await this.put(`/cleanListingAvailabilities/${id}`).catch(err => new ApolloError(toError(err)));
   };
 
   createBooking = async (booking: IBooking): Promise<IBooking> => {
-    return this.post(`/bookings`, booking).catch(err => new ApolloError(toError(err)));
+    return this.post(`/`, booking).catch(err => new ApolloError(toError(err)));
   };
 
   timeoutBooking = async (bookingId: String): Promise<IBooking> => {
-    return this.put(`/bookings/timeout/${bookingId}`).catch(err => new ApolloError(toError(err)));
+    return this.put(`/timeout/${bookingId}`).catch(err => new ApolloError(toError(err)));
   };
 
   getPendingBookingsByUser = async (args: any): Promise<IBooking> => {
-    return this.get(`/bookings/getPendingByGuestId/${args.userId}/${args.listingId}`).catch(err => new ApolloError(toError(err)));
+    return this.get(`/getPendingByGuestId/${args.userId}/${args.listingId}`).catch(err => new ApolloError(toError(err)));
   };
 
   getAllBookingsByUser = async (args: any): Promise<IBooking> => {
     if (args.userType === "host")
-      return this.get(`/bookings/byHostId/${args.userId}`).catch(err => new ApolloError(toError(err)));
-    return this.get(`/bookings/byGuestId/${args.userId}`).catch(err => new ApolloError(toError(err)));
+      return this.get(`/byHostId/${args.userId}`).catch(err => new ApolloError(toError(err)));
+    return this.get(`/byGuestId/${args.userId}`).catch(err => new ApolloError(toError(err)));
   };
 
 }
