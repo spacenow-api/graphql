487230e2ed111b4d4847a1a82991f1dca943f6ec
Arthemus <arthemus.moreira@gmail.com>
2019-09-30 12:03:32 +1000

    Search by limit and radius.

--- src/services/api-search.ts
+++ src/services/api-search.ts
@@ -13,24 +13,29 @@ class SearchAPI extends PersonalizationAPI {
     this.baseURL = apiAddress;
   }
 
-  searchByAddress = async (lat: string, lng: string, categories: string, duration: string, priceMin: number, priceMax: number, instant: string) => {
+  searchByAddress = async (lat: string, lng: string, categories: string, duration: string, priceMin: number, priceMax: number, instant: string, page: number, limit: number, radius: number) => {
     return this.post(`/search/listings/${lat},${lng}`, {
       categories,
       duration,
       priceMin,
       priceMax,
-      instant
+      instant,
+      page,
+      limit,
+      radius
     }).catch((err) => new ApolloError(toError(err)));
   };
 
-  searchByFilters = async (key: string, categories: string, duration: string, priceMin: number, priceMax: number, instant: string, page: number) => {
+  searchByFilters = async (key: string, categories: string, duration: string, priceMin: number, priceMax: number, instant: string, page: number, limit: number, radius: number) => {
     return this.post(`/search/${key}/query`, {
       categories,
       duration,
       priceMin,
       priceMax,
       instant,
-      page
+      page,
+      limit,
+      radius
     }).catch((err) => new ApolloError(toError(err)));
   };
 }
