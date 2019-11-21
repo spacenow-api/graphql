496156b205d5fa4c6731514a333cadfaba255006
Arthemus <arthemus.moreira@gmail.com>
2019-11-22 10:26:39 +1100

    Defining encoding and cache control on datasource request.

--- src/interfaces/personalization.inteface.ts
+++ src/interfaces/personalization.inteface.ts
@@ -3,6 +3,9 @@ import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
 export default class PersonalizationAPI extends RESTDataSource {
 
   willSendRequest(request: RequestOptions) {
-    request.headers.set('Authorization', this.context.token);
+    const { headers } = request;
+    headers.set('Authorization', this.context.token);
+    headers.set('Accept-Encoding', 'br,gzip,deflate');
+    headers.set('Cache-Control', 'private, max-age=1195723');
   }
 }
