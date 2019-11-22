dfd97cab9faad72a63186ec08d06a241c0c52309
Arthemus <arthemus.moreira@gmail.com>
2019-11-22 12:05:44 +1100

    Returning simple headers.

--- src/interfaces/personalization.inteface.ts
+++ src/interfaces/personalization.inteface.ts
@@ -3,9 +3,6 @@ import { RESTDataSource, RequestOptions } from 'apollo-datasource-rest';
 export default class PersonalizationAPI extends RESTDataSource {
 
   willSendRequest(request: RequestOptions) {
-    const { headers } = request;
-    headers.set('Authorization', this.context.token);
-    headers.set('Accept-Encoding', 'br,gzip,deflate');
-    headers.set('Cache-Control', 'private, max-age=1195723');
+    request.headers.set('Authorization', this.context.token);
   }
 }
