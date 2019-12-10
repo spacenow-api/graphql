9cce4f6c599e4806826f1e156621a9f4e1fab506
Arthemus <arthemus.moreira@gmail.com>
2019-12-10 14:19:27 +1100

    Removing mutation sendEmail not more used.

--- src/@graphql/index.ts
+++ src/@graphql/index.ts
@@ -2,7 +2,6 @@ import rootT from './typeDefs'
 import listingT from './listing/typeDefs'
 import paymentsT from './payments/typeDefs'
 import usersT from './users/typeDefs'
-import assetsT from './assets/typeDefs'
 import integrationsT from './integrations/typeDefs'
 import pricesT from './prices/typeDefs'
 import bookingT from './bookings/typeDefs'
@@ -17,7 +16,6 @@ const typeDefs = [
   listingT,
   paymentsT,
   usersT,
-  assetsT,
   integrationsT,
   pricesT,
   bookingT,
@@ -31,7 +29,6 @@ const typeDefs = [
 import listingR from './listing/resolvers'
 import paymentsR from './payments/resolvers'
 import usersR from './users/resolvers'
-import assetsR from './assets/resolvers'
 import integrationsR from './integrations/resolvers'
 import pricesR from './prices/resolvers'
 import bookingR from './bookings/resolvers'
@@ -45,7 +42,6 @@ const resolvers: any = [
   listingR,
   paymentsR,
   usersR,
-  assetsR,
   integrationsR,
   pricesR,
   bookingR,
